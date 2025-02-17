55ad60a1389b6596e6ab1a25d0c4cd5ff96f5814
Azhar Arrozak <azhardrozak2001@upi.edu>
2025-02-17 21:46:31 +0700

    feat: add UI login pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
 
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/auth/login" element={<Login />} />
+      <Route path="/auth/register" element={<Register />} />
     </Routes>
   )
 }
---
+++ src/layouts/DashboardLayouts.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DashboardLayouts = ({children}) => {
+  return (
+    <>DashboardLayouts</>
+  )
+}
+
+export default DashboardLayouts
